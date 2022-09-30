123c30f58b48e28f3781f512900fb9f1074a832c
Brian Nkhata <bnkhata@mra.mw>
2022-09-30 10:32:09 +0200

    WIP

--- Forms/FormFilterReport.cs
+++ Forms/FormFilterReport.cs
@@ -1,6 +1,7 @@
 ﻿using DevExpress.XtraEditors;
 using DevExpress.XtraReports.UI;
 using Katswiri.Data;
+using Katswiri.Enums;
 using Katswiri.Reports;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,9 @@ namespace Katswiri.Forms
         public FormFilterReport()
         {
             InitializeComponent();
+            loadCustomers();
+            loadCashiers();
+            loadSaleTypes();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,6 +34,51 @@ namespace Katswiri.Forms
                 ReportPrintTool printTool = new ReportPrintTool(salesReport);
                 //printTool.ShowPreviewDialog();
                 printTool.ShowPreview();
+            }
+        }
+
+        private void loadCustomers()
+        {
+            using (db = new BEntities())
+            {
+                lookUpEditCustomer.Properties.DataSource = db.Users.Where(x=>x.UserType == "Customer").ToList();
+                lookUpEditCustomer.Properties.ValueMember = "UserId";
+                lookUpEditCustomer.Properties.DisplayMember = "Name";
+                lookUpEditCustomer.Properties.NullText = "Customer";
+            }
+        }
+
+        private void loadCashiers()
+        {
+            using (db = new BEntities())
+            {
+                lookUpEditCashier.Properties.DataSource = db.Users.Where(x => x.UserType == "Staff").ToList();
+                lookUpEditCashier.Properties.ValueMember = "UserId";
+                lookUpEditCashier.Properties.DisplayMember = "Name";
+                lookUpEditCashier.Properties.NullText = "Staff";
+            }
+        }
+
+        private void loadSaleTypes()
+        {
+            using (db = new BEntities())
+            {
+                Dictionary<int, string> saleType = Enum.GetValues(typeof(SaleType)).Cast<SaleType>().ToDictionary(x => (int)x, x => x.ToString());
+                lookUpEditSaleType.Properties.DataSource = saleType;
+                lookUpEditSaleType.Properties.ValueMember = "Value";
+                lookUpEditSaleType.Properties.DisplayMember = "Value";
+                lookUpEditSaleType.Properties.NullText = "Sale Type";
+            }
+        }
+
+        private void PaymentTypes()
+        {
+            using (db = new BEntities())
+            {
+                lookUpEditPaymentType.Properties.DataSource = db.PaymentTypes.Where(x => x.Deleted == 0).ToList();
+                lookUpEditPaymentType.Properties.ValueMember = "Value";
+                lookUpEditPaymentType.Properties.DisplayMember = "Value";
+                lookUpEditPaymentType.Properties.NullText = "Payment Type";
             }
         }
     }
